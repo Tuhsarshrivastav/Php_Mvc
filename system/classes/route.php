3d46a13d82b9fca63ec8c3ca9ea912762a1ee914
Tusharshrivastav <ts3657@gmail.com>
2021-07-29 09:03:21 +0530

    setup url

--- system/classes/route.php
+++ system/classes/route.php
@@ -2,8 +2,18 @@
   class route {
       public function __construct()
       {
-          echo "Route class";
+        $url =  $this->url();
+        print_r($url);
+      }
+
+     public function url()
+      {
+        if(isset($_GET['url'])){
+          $url = $_GET['url'];
+          $url  = rtrim($url);
+          $url  =filter_var($url,FILTER_SANITIZE_URL);
+          $url  = explode("/",$url);
+          return $url;
+        }
       }
   }
-
-?>
